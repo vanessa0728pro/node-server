8c7d163840ff50442a8eb08f3ecdf6982fe195b8
Vanessa Forero Olarte <vanessa_0728@hotmail.com>
2023-07-18 09:49:25 -0500

    Mi producto

---
+++ review-1.md
@@ -0,0 +1,17 @@
+¿Qué es mi producto y para qué sirve?
+
+Mi producto es un programa que le permite administrar una lista de tareas. Puede agregar nuevas tareas, eliminar tareas existentes, marcar tareas como completadas y mostrar todas las tareas en la lista. Es una herramienta útil para organizar y hacer un seguimiento de sus tareas pendientes.
+
+¿Cuáles son las funcionalidades más importantes y por qué los usuarios las usarían?
+
+Agregar tareas: Con esta función, puede agregar nuevas tareas a su lista. El programa le solicitará un indicador y una descripción para la nueva tarea. Esto es útil para capturar rápidamente las tareas que necesita realizar.
+
+Eliminar tareas: Si ya ha completado una tarea o deseas eliminar una que ya no es relevante, puede utilizar esta función para eliminarla de la lista. Solo necesita proporcionar el índice de la tarea que desea eliminar.
+
+Completar tareas: Al marcar una tarea como completada, puede llevar un registro de sus logros. Esta función le permite marcar una tarea específica como completada, lo que le ayuda a visualizar su progreso y mantener la lista actualizada.
+
+Mostrar tareas: Esta función muestra todas las tareas en su lista, junto con su estado de completado. Puede ver rápidamente qué tareas aún están pendientes y cuáles ha completado.
+
+Encontrara estas funcionalidades útiles para organizar sus actividades diarias, mantener un seguimiento de sus tareas y asegurarse de no olvidar ninguna tarea importante.
+
+En resumen, mi producto es un programa que le permite administrar una lista de tareas, agregar nuevas tareas, eliminar tareas existentes, marcar tareas como completadas y mostrar todas las tareas en la lista. Es una herramienta útil para organizar y realizar un seguimiento de tus tareas pendientes de manera eficiente.
---
+++ server.js
@@ -0,0 +1,108 @@
+const http = require("http");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const tareas = [];
+
+function agregarTarea() {
+  return new Promise((resolve) => {
+    rl.question("Indicador: ", (indicador) => {
+      rl.question("Descripción: ", (descripcion) => {
+        tareas.push({ indicador, descripcion, completada: false });
+        console.log("Tarea agregada exitosamente.");
+        resolve();
+      });
+    });
+  });
+}
+
+function eliminarTarea() {
+  return new Promise((resolve) => {
+    rl.question("Índice de la tarea a eliminar: ", (indice) => {
+      if (indice >= 0 && indice < tareas.length) {
+        tareas.splice(indice, 1);
+        console.log("Tarea eliminada exitosamente.");
+      } else {
+        console.log("Índice inválido.");
+      }
+      resolve();
+    });
+  });
+}
+
+function completarTarea() {
+  return new Promise((resolve) => {
+    rl.question("Índice de la tarea completada: ", (indice) => {
+      if (indice >= 0 && indice < tareas.length) {
+        tareas[indice].completada = true;
+        console.log("Tarea completada.");
+      } else {
+        console.log("Índice inválido.");
+      }
+      resolve();
+    });
+  });
+}
+
+function mostrarTareas() {
+  console.log("Lista de tareas:");
+  tareas.forEach((tarea, indice) => {
+    console.log(
+      `${indice}: [${tarea.completada ? "x" : " "}] ${tarea.indicador} - ${
+        tarea.descripcion
+      }`
+    );
+  });
+}
+
+async function mostrarMenu() {
+  console.log("\n== Menú ==");
+  console.log("1. Agregar tarea");
+  console.log("2. Eliminar tarea");
+  console.log("3. Completar tarea");
+  console.log("4. Mostrar tareas");
+  console.log("5. Salir");
+
+  const opcion = await new Promise((resolve) => {
+    rl.question("Seleccione una opción: ", (input) => {
+      resolve(input);
+    });
+  });
+
+  switch (opcion) {
+    case "1":
+      await agregarTarea();
+      break;
+    case "2":
+      await eliminarTarea();
+      break;
+    case "3":
+      await completarTarea();
+      break;
+    case "4":
+      mostrarTareas();
+      break;
+    case "5":
+      rl.close();
+      return;
+    default:
+      console.log("Opción inválida.");
+  }
+
+  await mostrarMenu();
+}
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(tareas));
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+  mostrarMenu();
+});
